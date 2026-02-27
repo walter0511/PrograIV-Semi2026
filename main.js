2a61e0f84bf98069cafc927bac366371ac9ed745
walter0511 <walterbenavidesernesto@gmail.com>
2026-02-27 10:33:22 -0600

    avance de parcial

--- main.js
+++ main.js
@@ -1,71 +1,62 @@
-const { createApp } = Vue;
+const { createApp } = Vue,
+    Dexie = window.Dexie,
+    sha256 = CryptoJS.SHA256;
+
+     window.db = new Dexie("db_academica");
+    window.db.version(2).stores({
+        alumnos: "++idAlumno,codigo,nombre,direccion,email,telefono,municipio,departamento,fecha_de_nacimiento,sexo,hash",
+        materias: "++idMateria,codigo,nombre,uv,hash",
+        docentes: "++idDocente,codigo,nombre,direccion,email,telefono,escalafon,hash",
+        matriculas: "++idMatricula,codigo_alumno,ciclo_periodo,hash",
+        inscripciones: "++idInscripcion,codigo_alumno,codigo_materia,fecha_inscripcion,ciclo_periodo,hash",
+    });
 
 createApp({
-    data() {
-        return {
-            alumno: {
-                codigo: "", nombre: "", direccion: "", municipio: "",
-                departamento: "", telefono: "", fechaNacimiento: "",
-                sexo: "", email: ""
-            },
-            accion: 'nuevo',
-            id: 0,
-            buscar: '',
-            alumnos: []
+    components:{
+        alumnos,
+        busqueda_alumnos,
+        materias,
+        busqueda_materias,
+        docentes,
+        busqueda_docentes,
+        matriculas,
+        busqueda_matriculas,
+        inscripciones,
+        busqueda_inscripciones
+        
+    },
+    data(){
+        return{
+            forms:{
+                alumnos:{mostrar:false},
+                busqueda_alumnos:{mostrar:false},
+                materias:{mostrar:false},
+                busqueda_materias:{mostrar:false},
+                docentes:{mostrar:false},
+                busqueda_docentes:{mostrar:false},
+                matriculas:{mostrar:false},
+                busqueda_matriculas:{mostrar:false},
+                inscripciones:{mostrar:false},
+                busqueda_inscripciones:{mostrar:false}
+            }
         }
     },
-    methods: {
-        obtenerAlumnos() {
-            let n = localStorage.length;
-            this.alumnos = [];
-            for (let i = 0; i < n; i++) {
-                let key = localStorage.key(i);
-                if (!isNaN(key)) {
-                    let data = JSON.parse(localStorage.getItem(key));
-                    if (data.nombre.toUpperCase().includes(this.buscar.toUpperCase()) ||
-                        data.codigo.toUpperCase().includes(this.buscar.toUpperCase())) {
-                        this.alumnos.push(data);
-                    }
-                }
-            }
+    methods:{
+        buscar(ventana, metodo){
+            this.$refs[ventana][metodo]();
         },
-        eliminarAlumno(id, e) {
-            e.stopPropagation();
-            if (confirm("¿Está seguro de eliminar este registro?")) {
-                localStorage.removeItem(id);
-                this.obtenerAlumnos();
-                if(this.id === id) this.limpiarFormulario();
-            }
-        },
-        modificarAlumno(alumno) {
-            this.accion = 'modificar';
-            this.id = alumno.id;
-            // Copiamos los datos al formulario
-            Object.assign(this.alumno, alumno);
-        },
-        guardarAlumno() {
-            let currentId = this.accion == 'modificar' ? this.id : new Date().getTime();
-            let datos = { id: currentId, ...this.alumno };
+     abrirVentana(ventana){
+    const estadoActual = this.forms[ventana].mostrar;
 
-            if(this.accion == 'nuevo' && this.buscarAlumno(datos.codigo)) {
-                alert("Este código ya pertenece a otro alumno.");
-                return;
-            }
+    Object.keys(this.forms).forEach(key => {
+        this.forms[key].mostrar = false;
+    });
 
-            localStorage.setItem(currentId, JSON.stringify(datos));
-            this.limpiarFormulario();
-            this.obtenerAlumnos();
-        },
-        limpiarFormulario() {
-            this.accion = 'nuevo';
-            this.id = 0;
-            Object.keys(this.alumno).forEach(key => this.alumno[key] = "");
-        },
-        buscarAlumno(codigo) {
-            return this.alumnos.find(a => a.codigo.trim().toUpperCase() === codigo.trim().toUpperCase());
+    this.forms[ventana].mostrar = !estadoActual;
+},
+        modificar(ventana, metodo, data){
+            this.$refs[ventana][metodo](data);
         }
     },
-    mounted() {
-        this.obtenerAlumnos();
-    }
+   
 }).mount("#app");
